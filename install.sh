9e2b0f6f5b18c376cfe4da9b64ac18f86087af79
Andrii K <andrii.komarychev@gmail.com>
2026-02-20 17:30:04 -0300

    Add Ghostty configuration file and update install script to create symlink

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ ln -sf $DOTFILES/.zshrc ~/.zshrc
 ln -sf $DOTFILES/.gitconfig ~/.gitconfig
 # Link VSCode settings (Cursor)
 ln -sf ~/projects/dotfiles/vscode/settings.json ~/Library/Application\ Support/Cursor/User/settings.json
+# Link Ghostty config
+ln -sf ~/projects/dotfiles/ghostty/config ~/.config/ghostty/config
