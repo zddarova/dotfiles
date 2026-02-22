52e92fa41551cf776fd3046c5e6313af720d346e
Andrii K <andrii.komarychev@gmail.com>
2026-02-22 16:41:21 -0300

    Update install script to link Ghostty config in macOS Application Support and modify theme settings in Ghostty config file

--- install.sh
+++ install.sh
@@ -7,8 +7,9 @@ ln -sf $DOTFILES/.zshrc ~/.zshrc
 ln -sf $DOTFILES/.gitconfig ~/.gitconfig
 # Link VSCode settings (Cursor)
 ln -sf $DOTFILES/vscode/settings.json ~/Library/Application\ Support/Cursor/User/settings.json
-# Link Ghostty config
-ln -sf $DOTFILES/ghostty/config ~/.config/ghostty/config
+# Link Ghostty config (Ghostty on macOS loads from Application Support first)
+mkdir -p "$HOME/Library/Application Support/com.mitchellh.ghostty"
+ln -sf $DOTFILES/ghostty/config "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
 # Link Tmux config
 ln -sf $DOTFILES/.tmux.conf ~/.tmux.conf
 
